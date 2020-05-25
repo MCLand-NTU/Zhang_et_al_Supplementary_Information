960b5a35382f3bfaf32de58109891a8740a81e5d
MCLand-NTU <kethingchong@gmail.com>
2020-05-25 16:58:38 +0800

    Update GetAllTrajectories.m

--- Additional_file_2_Supplementary_Code_Zhang_et_al/ODEs/common_code/GetAllTrajectories.m
+++ Additional_file_2_Supplementary_Code_Zhang_et_al/ODEs/common_code/GetAllTrajectories.m
@@ -1,11 +1,11 @@
 function res = GetAllTrajectories(initialConditions, t)
 % Use a loop to calculate all the trajectories or time course simulations
 disp('Start calculations ...')
-res = cell(size(initialConditions, 1),1);
+res = cell(size(initialConditions, 1),1); % size(x,1), return the number of rows
 for i = 1:size(initialConditions, 1)
     % Show the progress of the calculation 
     disp(i);
     % Calculate the time course simulation according to each initial
     % conditions
-    res{i} = GetOneTrajectory(t, initialConditions(i, :));
+    res{i} = GetOneTrajectory(t, initialConditions(i, :)); % only initial conditions for one trajectory (the number of genes)
 end
