188cda8456ffb4943c7a2b2d16794e4eb1ea8f4f
MCLand-NTU <kethingchong@gmail.com>
2020-05-25 16:38:05 +0800

    Update DrawLandscape.m

--- Additional_file_2_Supplementary_Code_Zhang_et_al/ODEs/common_code/DrawLandscape.m
+++ Additional_file_2_Supplementary_Code_Zhang_et_al/ODEs/common_code/DrawLandscape.m
@@ -9,53 +9,68 @@ allTrajectories = GetAllTrajectories(initialConditions, t);
 figure;
 
 % Get the probability distributions
-positionProbabilities = GetPositionProbabilities(fromInitialCondition, toInitialCondition, index, splitNumber, allTrajectories);
+[positionProbabilities,XMin,XMax,YMin,YMax] = GetPositionProbabilities(fromInitialCondition, toInitialCondition, index, splitNumber, allTrajectories, exe_dr);
 
 % Normalized Probability
-normalized_Prob=positionProbabilities./(sum(sum(positionProbabilities)));
+normalized_Prob=positionProbabilities./(sum(sum(positionProbabilities))); % each grid points/all points
 
 % U = - ln P
 Neg_lnP=-log(normalized_Prob); 
 
-[x y] = meshgrid(linspace(fromInitialCondition(index(1)), toInitialCondition(index(1)), splitNumber), ...
-    linspace(fromInitialCondition(index(2)), toInitialCondition(index(2)), splitNumber));
+[x y] = meshgrid(linspace(XMin, XMax, splitNumber), ...
+    linspace(YMin, YMax, splitNumber));
 
 % plot the 3D view of the potential landscape
 surf(x,y,Neg_lnP);
 
 % label the axes
-xlabel(variableNames(index(2)));
-ylabel(variableNames(index(1)));
+if exe_dr== true
+    xlabel('Component 1');
+    ylabel('Component 2');
+else
+    xlabel(variableNames(index(2)));
+    ylabel(variableNames(index(1)));
+end
 zlabel('U')
 
 % set the x and y axes
-axis([range_min range_max range_min range_max]);
+axis([XMin XMax YMin YMax]);
 
 % 3D view
 view(-13,66); 
 box on;
 
 % set the color bar 
-colorbar;
+colormap('jet');colorbar;
 
+% save img
+saveas(gcf,'../Results/3D view.fig')
 %%
 
 % Plot Top view of the potential landscape
 figure;
 
 surf(x,y,Neg_lnP);
-xlabel(variableNames(index(2)));
-ylabel(variableNames(index(1)));
+if exe_dr== true
+    xlabel('Component 1');
+    ylabel('Component 2');
+else
+    xlabel(variableNames(index(2)));
+    ylabel(variableNames(index(1)));
+end
 zlabel('U')
 
-axis([range_min range_max range_min range_max]);
+axis([XMin XMax YMin YMax]);
 
 % Top view
 view(0,90);
 box on;
-colorbar;
+colormap('jet');colorbar;
 
-% show how long it takes to calculate and plot the landscape
+% save img
+saveas(gcf,'../Results/Top view.fig')
+
+% time: show how long it takes to calculate and plot the landscape
 disp('Calculation and plotting finished.')
 duration=toc/(60*60);
 disp(['It took ' num2str(duration) ' hours'])
