ab0258126a400c5af6a38e00ec7acc7e82fc6ed8
MCLand-NTU <kethingchong@gmail.com>
2020-05-25 16:59:59 +0800

    Update GetOneTrajectory.m

--- Additional_file_2_Supplementary_Code_Zhang_et_al/ODEs/common_code/GetOneTrajectory.m
+++ Additional_file_2_Supplementary_Code_Zhang_et_al/ODEs/common_code/GetOneTrajectory.m
@@ -1,3 +1,3 @@
 function res = GetOneTrajectory(t, initialCondition)
-% Calculate the time course simulations using MATLAB built in function ode45 
+% Calculate the time course simulations using MATLAB built in function ode45, get the numerical solution. 
 [T res] = ode45(@equations, t,  initialCondition, []);
